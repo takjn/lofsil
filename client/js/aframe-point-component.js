1b1762001330a6a9f3218e468a37eef39e31df95
Jun Takeda <takeda.jun@gmail.com>
2017-09-29 22:00:07 +0900

    WIP

--- client/js/aframe-point-component.js
+++ client/js/aframe-point-component.js
@@ -44,94 +44,105 @@
 /* 0 */
 /***/ (function(module, exports) {
 
-	/* global AFRAME THREE */
+  /* global AFRAME THREE */
 
-	if (typeof AFRAME === 'undefined') {
-	  throw new Error('Component attempted to register before AFRAME was available.');
-	}
+  if (typeof AFRAME === 'undefined') {
+    throw new Error('Component attempted to register before AFRAME was available.');
+  }
 
-	/**
-	 * Point component for A-Frame.
-	 */
-	AFRAME.registerComponent('point', {
-	  schema: {
-	    size: {
-	      type: 'number',
-	      default: 1
-	    },
-	    perspective: {
-	      type: 'boolean',
-	      default: true
-	    },
-	    opacity: {
-	      type: 'number',
-	      default: 1.0
-	    },
-	    transparent: {
-	      type: 'boolean',
-	      default: false
-	    }
-	},
+  /**
+   * Heatmap component for A-Frame.
+   */
+  AFRAME.registerComponent('point', {
+    schema: {
+      size: {
+        type: 'number',
+        default: 1
+      },
+      perspective: {
+        type: 'boolean',
+        default: true
+      },
+      opacity: {
+        type: 'number',
+        default: 1.0
+      },
+      transparent: {
+        type: 'boolean',
+        default: false
+      }
+    },
 
-	  /**
-	   * Set if component needs multiple instancing.
-	   */
-	  multiple: false,
+    /**
+     * Set if component needs multiple instancing.
+     */
+    multiple: false,
 
-	  /**
-	   * Called once when component is attached. Generally for initial setup.
-	   */
-	  init: function () {
-	  },
+    /**
+     * Called once when component is attached. Generally for initial setup.
+     */
+    init: function () {
+      this.material = new THREE.PointsMaterial({
+        size: this.data.size, 
+        sizeAttenuation: this.data.perspective,
+        opacity: this.data.opacity, 
+        transparent: this.data.transparent, 
+        vertexColors: THREE.VertexColors
+      });
+    },
 
-	  setPoints: function (points) {
-			const pallet = [60, 55, 50, 45, 40, 35, 30, 25, 20, 15, 0];			
-			var colors = [];
+    setPoints: function (points) {
+      const pallet = [
+        "hsl(60, 100%, 50%)",
+        "hsl(55, 100%, 50%)",
+        "hsl(50, 100%, 50%)",
+        "hsl(45, 100%, 50%)",
+        "hsl(40, 100%, 50%)",
+        "hsl(35, 100%, 50%)",
+        "hsl(30, 100%, 50%)",
+        "hsl(25, 100%, 50%)",
+        "hsl(20, 100%, 50%)",
+        "hsl(15, 100%, 50%)",
+        "hsl(0, 100%, 50%)"
+      ];
+      var colors = [];
 
-	    this.geometry = new THREE.Geometry();
-	    var vertices = this.geometry.vertices;
-	    points.forEach(function (point) {
-				vertices.push(new THREE.Vector3(point[0], point[1], point[2]));
-				var c = "hsl(" + pallet[point[3]] + ", 100%, 50%)";
-				colors.push(new THREE.Color(c));
-			});
-			this.geometry.colors = colors;
+      this.geometry = new THREE.Geometry();
+      var vertices = this.geometry.vertices;
+      points.forEach(function (point) {
+        vertices.push(new THREE.Vector3(point[0], point[1], point[2]));
+        colors.push(new THREE.Color(pallet[point[3]]));
+      });
+      this.geometry.colors = colors;
 
-			this.material = new THREE.PointsMaterial({
-				size: this.data.size, 
-				sizeAttenuation: this.data.perspective,
-				opacity: this.data.opacity, 
-				transparent: this.data.transparent, 
-				vertexColors: THREE.VertexColors
-			});
-			
-			// Create mesh.
-	    this.points = new THREE.Points(this.geometry, this.material);
-	    // Set mesh on entity.
-	    this.el.setObject3D('mesh', this.points);
-	  },
+      // Create mesh.
+      this.points = new THREE.Points(this.geometry, this.material);
+      
+      // Set mesh on entity.
+      this.el.setObject3D('mesh', this.points);
+    },
 
-	  /**
-	   * Called when a component is removed (e.g., via removeAttribute).
-	   * Generally undoes all modifications to the entity.
-	   */
-	  remove: function () {
-	    this.el.removeObject3D('mesh');
-	  }
+    /**
+     * Called when a component is removed (e.g., via removeAttribute).
+     * Generally undoes all modifications to the entity.
+     */
+    remove: function () {
+      this.el.removeObject3D('mesh');
+    }
 
-	});
+  });
 
-	AFRAME.registerPrimitive('a-point', {
-	  defaultComponents: {
-	    point: {}
-	  },
-	  mappings: {
-	    size: 'point.size',
-	    perspective: 'point.perspective',
-	    opacity: 'point.opacity',
-	    transparent: 'point.transparent',
-	  }
-	});
+  AFRAME.registerPrimitive('a-heatmap', {
+    defaultComponents: {
+      point: {}
+    },
+    mappings: {
+      size: 'point.size',
+      perspective: 'point.perspective',
+      opacity: 'point.opacity',
+      transparent: 'point.transparent',
+    }
+  });
 
 
 /***/ })
